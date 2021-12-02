355dcc9f895641525301358702de8861908e4fcc
grayTurtle01 <the.correo@protonmail.com>
2021-12-02 02:32:50 -0600

    add new post by new function create_post_node(post_data)

--- network/static/network/post.js
+++ network/static/network/post.js
@@ -10,38 +10,10 @@ document.querySelector('form').onsubmit = () => {
   
   .then( res => res.json())
   .then( post => {
-    div = document.createElement('div')
-    div.className = 'post text-left'
-    div.innerHTML = `
-    <div class="post-header mb-2">
-                
-      <div class="post-header-left">
 
-        <img class="post-image" src="${post.creator_image}" >
-        <a href="/profile/${post.creator}/1">
-            <h4>${ post.creator }</h4>
-        </a>
-      </div>
+    let post_node = create_post_node(post)
 
-      <small class='text-secondary'>${post.timestamp }</small>
-   </div>
-
-      <p class="ml-2">${post.message}</p>      
-      <textarea class="form-control" style="display: none;" ></textarea>
-      <button style="display: none;" class="btn btn-sm btn-info mt-1">Update</button>
-
-      <div class="post-footer">
-        <div class="likes-div" data-post_id="${post.id}">
-          <i class="fa fa-heart like"></i>
-          <span class="likes">${post.likes }</span> <br> 
-        </div>
-
-        <div class="edit-div">
-            <a class="btn btn-sm btn-info edit" data-post_id="${post.id}">Edit</a>
-      </div>
-      </div>
-    `
-    document.querySelector('#posts').prepend(div)
+    document.querySelector('#posts').prepend(post_node)
     document.querySelector('#message').value = ""
     load_likes()
     load_edit_buttons()
@@ -52,4 +24,40 @@ document.querySelector('form').onsubmit = () => {
   return false
 }
 
+function create_post_node(post_data){
+  let post = post_data
 
+  div = document.createElement('div')
+  div.className = 'post text-left'
+  div.innerHTML = `
+  <div class="post-header mb-2">
+              
+    <div class="post-header-left">
+
+      <img class="post-image" src="${post.creator_image}" >
+      <a href="/profile/${post.creator}/1">
+          <h4>${ post.creator }</h4>
+      </a>
+    </div>
+
+    <small class='text-secondary'>${post.timestamp }</small>
+ </div>
+
+    <p class="ml-2">${post.message}</p>      
+    <textarea class="form-control" style="display: none;" ></textarea>
+    <button style="display: none;" class="btn btn-sm btn-info mt-1">Update</button>
+
+    <div class="post-footer">
+      <div class="likes-div" data-post_id="${post.id}">
+        <i class="fa fa-heart like"></i>
+        <span class="likes">${post.likes }</span> <br> 
+      </div>
+
+      <div class="edit-div">
+          <a class="btn btn-sm btn-info edit" data-post_id="${post.id}">Edit</a>
+    </div>
+    </div>
+  `
+  return div
+
+}
