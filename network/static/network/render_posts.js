ac67f0824c183193498fe1403a911f6ba9c44c2e
grayTurtle01 <the.correo@protonmail.com>
2021-12-02 08:45:49 -0600

    paginator for all-post on spa

--- network/static/network/render_posts.js
+++ network/static/network/render_posts.js
@@ -1,6 +1,7 @@
 function render_posts(posts){
   
   let posts_node = document.querySelector('#posts')
+  posts_node.innerHTML = ''
   for( let post of posts ){
     let post_node = create_post_node(post)
     posts_node.append(post_node)
