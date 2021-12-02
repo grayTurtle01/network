e5730ec6562cc635713e90511894e21fb20e6618
grayTurtle01 <the.correo@protonmail.com>
2021-12-02 00:14:57 -0600

    like new-post immediately

--- network/static/network/like_unlike.js
+++ network/static/network/like_unlike.js
@@ -1,36 +1,32 @@
-like_buttons = document.querySelectorAll('.like')
-for( like_btn of like_buttons ){
-  like_btn.onclick = function(){
-    post = this.parentNode
-    post_id = post.dataset.post_id
-    
-    fetch("/like_unlike",{
-      method: 'PUT',
-      body: JSON.stringify({
-        'post_id' : post_id
-      })
-    })
-    .then( res => res.json())
-    .then( data => {
-      // likes = post.querySelector('.likes')
-      // likes.innerText = `Likes: ${data.likes}`
-      // if( data.message == "Like added"){
-      //   this.innerText = 'Unlike'
-      // }
-      // else{
-      //   this.innerText = 'Like'
-      // }
-      likes = post.querySelector('.likes')
-      likes.innerText = data.likes
-      if( data.message == "Like added"){
-        this.style.color = 'red'
-      }
-      else{
-        this.style.color = 'black'
-      }
 
-    } )
-   
-    
+function load_likes(){
+  like_buttons = document.querySelectorAll('.like')
+  for( like_btn of like_buttons ){
+    like_btn.onclick = function(){
+      post = this.parentNode
+      post_id = post.dataset.post_id
+      console.log(post_id)
+      
+      fetch("/like_unlike",{
+        method: 'PUT',
+        body: JSON.stringify({
+          'post_id' : post_id
+        })
+      })
+      .then( res => res.json())
+      .then( data => {
+        likes = post.querySelector('.likes')
+        likes.innerText = data.likes
+        if( data.message == "Like added"){
+          this.style.color = 'red'
+        }
+        else{
+          this.style.color = 'black'
+        }
+
+      } )
+      
+    }
   }
 }
+load_likes()
--- network/static/network/post.js
+++ network/static/network/post.js
@@ -27,14 +27,19 @@ document.querySelector('form').onsubmit = () => {
    </div>
 
       <p class="ml-2">${post.message}</p>      
-      <textarea style="display: none;"></textarea>
+      <textarea class="form-control" style="display: none;" ></textarea>
       <button style="display: none;" class="btn btn-sm btn-info mt-1">Update</button>
 
-      <i class="fa fa-heart"></i>
-      <span>${post.likes }</span> <br> 
+      <div class="footer>
+        <div class="likes-div" data-post_id="${post.id}">
+          <i class="fa fa-heart like"></i>
+          <span class="likes">${post.likes }</span> <br> 
+        </div>
+      </div>
     `
     document.querySelector('#posts').prepend(div)
     document.querySelector('#message').value = ""
+    load_likes()
   })
   
 
@@ -42,52 +47,4 @@ document.querySelector('form').onsubmit = () => {
   return false
 }
 
-// edit_buttons = document.querySelectorAll('.edit')
-// for (button of edit_buttons){
-//     button.onclick = function(){
-
-//       edit_button = this
-//       edit_button.style.display = 'none'
-//       post_id = this.dataset.post_id
-
-//       post = this.parentNode
-//       p = post.querySelector('p')
-//       message = p.innerText
-
-//       edit_area = post.querySelector('textarea')
-//       edit_area.innerHTML = message
-//       p.style.display = 'none'
-
-//       edit_area.style.display = 'block'
-
-//       update_btn = post.querySelector('button')
-//       update_btn.style.display = 'inline'
-
-//       update_btn.onclick = function (){
-//         post = this.parentNode
-//         p.innerHTML = edit_area.value
-        
-//         new_message = edit_area.value
-
-//         fetch("/update_post",{
-//           'method': 'POST',
-//           'body': JSON.stringify({
-//             'post_id' : post_id,
-//             'new_message': new_message
-//           })
-//         })
-//         .then( res => res.text())
-//         .then( msg => console.log(msg))
-//         .then( () =>  { 
-//           edit_area.style.display = 'none'
-//           this.style.display = 'none'
-//           edit_button.style.display = 'inline-block'
-//           p.style.display = 'block'
-//         })  
-//       }   
-      
-
-
-//     }
-// }
 
